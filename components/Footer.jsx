0994aa9d0bb123c944e0542158e5394794e4d7b2
abdulrehmanmalick <abdworks75@gmail.com>
2024-03-26 06:01:01 +0500

    added a footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='py-10 px-20 hero flex justify-between'>
+      <div className='navigation__logo no-underline text-white text-5xl font-bold my-auto'>
+        LEGALBOT
+      </div>
+      <div className='text-white underline'>
+        <ul className='flex gap-5'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Chat</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
